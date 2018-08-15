ff6926a2f9dfc79f63997bcf5346c47fe9db1e2e
KARIYER\ahmet.aygun <faygun89@gmail.com>
2018-08-15 10:08:40 +0300

    api uri change.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '550132814d8950ee055b02f53f19abc8';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_REQUEST = 'FETCH_REQUEST';
 export const FETCH_WEATHER = 'FETCH_WEATHER';
